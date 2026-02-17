e3042a1b7a11fc965ac112304367f97e76f8bf10
shui-official <shui.officialtoken@gmail.com>
2026-02-17 15:14:42 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,19 +8,14 @@ import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
-// set custom RPC server endpoint for the final website
-// const endpoint = "https://explorer-api.devnet.solana.com";
-// const endpoint = "http://127.0.0.1:8899";
+// RPC endpoint (mainnet). You can replace later with your own paid RPC for better reliability.
 const endpoint = "https://ssc-dao.genesysgo.net";
 
-const WalletProvider = dynamic(
-  () => import("../contexts/ClientWalletProvider"),
-  {
-    ssr: false,
-  }
-);
+const WalletProvider = dynamic(() => import("../contexts/ClientWalletProvider"), {
+  ssr: false,
+});
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider>
@@ -29,5 +24,3 @@ function MyApp({ Component, pageProps }: AppProps) {
     </ConnectionProvider>
   );
 }
-
-export default MyApp;
