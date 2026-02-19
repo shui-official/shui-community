092b4a16dba5541b904543db445c99a01d9bb23b
shui.official <262105358+shui-official@users.noreply.github.com>
2026-02-19 07:25:12 +0100

    Fix WalletModalProvider context (setVisible)

--- src/contexts/ClientWalletProvider.tsx
+++ src/contexts/ClientWalletProvider.tsx
@@ -1,48 +1,26 @@
-import type { WalletProviderProps } from "@solana/wallet-adapter-react";
-import { WalletProvider } from "@solana/wallet-adapter-react";
-
-import {
-  getPhantomWallet,
-  // getLedgerWallet,
-  // getMathWallet,
-  getSolflareWallet,
-  getSolletWallet,
-  // getSolongWallet,
-} from '@solana/wallet-adapter-wallets'
-import { useMemo } from "react";
+import React, { useMemo } from "react";
+import { WalletProvider, WalletProviderProps } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
-import('@solana/wallet-adapter-react-ui/styles.css' as any) ;
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { SolflareWalletAdapter } from "@solana/wallet-adapter-solflare";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export function ClientWalletProvider(
   props: Omit<WalletProviderProps, "wallets">
 ): JSX.Element {
   const wallets = useMemo(
-    () => [
-      getPhantomWallet(),
-      getSolflareWallet(),
-      // getTorusWallet({
-      //   options: {
-      //     // TODO: Get your own tor.us wallet client Id
-      //     clientId:
-      //       "BOM5Cl7PXgE9Ylq1Z1tqzhpydY0RVr8k90QQ85N7AKI5QGSrr9iDC-3rvmy0K_hF0JfpLMiXoDhta68JwcxS1LQ",
-      //   },
-      // }),
-      // getLedgerWallet(),
-      // getSolongWallet(),
-      // getMathWallet(),
-      getSolletWallet(),
-    ],
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
     []
   );
 
   return (
     <WalletProvider wallets={wallets} {...props}>
-      <WalletModalProvider {...props} />
+      <WalletModalProvider />
+      {props.children}
     </WalletProvider>
   );
-
-
 }
 
 export default ClientWalletProvider;
